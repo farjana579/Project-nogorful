f82766f087155c363bc1a56db83f4d8e8b042f0e
farjanaabedin <farjanaabedin579@gmail.com>
2021-11-14 21:46:31 +0600

    new object

--- object.js
+++ object.js
@@ -1,13 +1,49 @@
-class Student{
-constructor(sRoll,sName,sMotherName,sFatherName,sClass,sAdmissionDate,sAddress){
-    this.roll = sRoll;
-    this.name = sName;
-    this.motherName = sMotherName;
-    this.fatherName = sFatherName;
-    this.class = sClass;
-    this.admissionDate = sAdmissionDate;
-    this.address = sAddress;
+ 
+const student =  {
+    roll:01,
+    name: "Farjana Abedin",
+    motherName: "Mano Ara",
+    fatherName: "joynal abedin",
+    class:5,
+    admissionDate:"11-11-2021",
+    address:'Muradpur',
 }
+
+
+
+//object for teacher
+
+
+const teacher = {
+    id:01,
+    name:"Fahim Foisal",
+    position:"Assistant teacher",
+    study:"Chittagong university",
+    phone:018324567645,
+    mail:"fahimfoishal@cu.com",
+    address:"Hamzarbag",
+    joinDate:"11-10-2020"
+
+
 }
-const student1 = new Student(1,"Farjana",'ManoAra',"Joynal",5,"11-11-2021","muradpur");
-console.log(student1.roll);
+
+
+//object for one child one donor
+const donor = {
+    id:01,
+    name:"Julekha Begum",
+    phone:01678946574,
+    address : "GEC",
+    Salary: 250000;
+}
+
+
+const child = {
+    id:01,
+    name:"Shiuli",
+    motherName: "Sultana ara",
+    fatherName: "Rahim uddin",
+    address : "Sholoshor",
+    parentsPhone:01678908523,
+    study: "xyz school";
+}
